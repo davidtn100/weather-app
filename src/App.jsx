a3bb53fec8781942a03757875800a05347e2b3ea
davidtn100 <davidtn100@gmail.com>
2024-02-06 23:22:12 -0800

    Added nivo chart display. Example chart shown

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,32 @@ import ForecastWeatherCard from './ForecastWeatherCard'
 import './LocationInputForm.jsx'
 import LocationInputForm from './LocationInputForm'
 
+import MyResponsiveLine from './MyResponsiveLine'
+
 function App() {
   const [ForecastDay, setDisplayForecastDays] = useState(3)
   const [locationInput, setLocationInput] = useState(92841)
+
+  const data = [
+    {
+    "id": "us",
+    "color": "hsl(145, 70%, 50%)",
+    "data": [
+      { "x": "plane", "y": 162},
+      {"x": "helicopter","y": 250},
+      {"x": "boat","y": 253},
+      {"x": "train","y": 44},
+      {"x": "subway","y": 163},
+      {"x": "bus","y": 71},
+      {"x": "car","y": 101},
+      {"x": "moto","y": 290},
+      {"x": "bicycle","y": 47},
+      {"x": "horse","y": 125},
+      {"x": "skateboard","y": 131},
+      {"x": "others","y": 8}
+      ]
+    }
+  ];
 
   function populateForecastWeatherCards(){
     const totalForecastDays = ForecastDay
@@ -25,10 +48,19 @@ function App() {
     return forecastCards
   }
   return (
-    <div className="app">
-    <LocationInputForm/>
-    <CurrentWeatherCard location={locationInput}/>
-    {populateForecastWeatherCards()}
+    <div>
+
+      <div className="weatherCards">
+      <LocationInputForm/>
+      <CurrentWeatherCard location={locationInput}/>
+      {populateForecastWeatherCards()}
+      </div>
+
+      <div className="chart">
+      <h1>Super Cool Nivo Bar Chart</h1>
+      <MyResponsiveLine data={data} />
+      </div>
+
     </div>
   )
 }
