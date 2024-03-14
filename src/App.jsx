3833eb7ff959d04648cfb962dbd4a653535b1683
davidtn100 <davidtn100@gmail.com>
2024-03-13 19:56:11 -0700

    Fixed bug where chart data displayed did not update on first click. Chained useEffects with useStates to create asynchronous updating of states

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-
-import './CurrentWeatherCard.jsx'
-import CurrentWeatherCard from './CurrentWeatherCard'
-
-import './ForecastWeatherCards.jsx'
+import CurrentWeatherCard from './CurrentWeatherCard.jsx'
 import ForecastWeatherCards from './ForecastWeatherCards.jsx'
-
-import './WeatherDetailsDropDown.jsx'
 import WeatherDetailsDropDown from './WeatherDetailsDropDown.jsx'
-
-import './LocationInputForm.jsx'
-import LocationInputForm from './LocationInputForm'
-
-import './hooks/useFetchData.jsx'
-import useFetchData from './hooks/useFetchData.jsx';
-
+import LocationInputForm from './LocationInputForm.jsx'
+import useFetchData from './hooks/useFetchData.jsx'
 import MyResponsiveLine from './MyResponsiveLine'
 
 function App() {
@@ -26,71 +15,82 @@ function App() {
   const [forecastDataArray, setForecastDataArray] = useState([])
   const [loading, setLoading] = useState(true)
   const [clickedDropdown, setClickedDropdown] = useState("hour_temp_f")
-  const [cardIndex, setCardIndex] = useState(0)
+  const [cardIndex, setCardIndex] = useState(null)
   const [axisLeftLegend, setAxisLeftLegend] = useState("Temperature")
 
   const { DataArray } = useFetchData(`http://api.weatherapi.com/v1/forecast.json?key=f7af6cf12f7040ac9e404543230612&q=92841&days=${ForecastDays}`, ForecastDays);
 
+  useEffect(() => {
+    if (cardIndex !== null && clickedDropdown !== null) {
+      setSelectedCardData(DataArray[cardIndex][clickedDropdown]);
+    }
+  }, [cardIndex, clickedDropdown]);
+
   const handleCardClick = (event) => {
-    setCardIndex(event.currentTarget.getAttribute('index'))
-    if (cardIndex) {
-      console.log(`This card index is ${cardIndex}`)
+    const newIndex = event.currentTarget.getAttribute('index');
+    if (newIndex) {
+      console.log(`This card index is ${newIndex}`);
+      setCardIndex(newIndex);
     } else {
       console.error('Unable to determine card index');
     }
-
-    if(event.currentTarget.id === "currentWeatherCard"){
-      console.log("Current weather card clicked")
-    } else if (event.currentTarget.id === `forecastCard${cardIndex}`){
-      console.log(`Forecast Weather Card #${cardIndex} clicked!`)
-      setSelectedCardData(DataArray[cardIndex][clickedDropdown])
-    } else {
-      console.error("Unknown card clicked")
-    }
   }
+
+  useEffect(() => {
+    if (cardIndex !== null && clickedDropdown !== null) {
+        setSelectedCardData(DataArray[cardIndex][clickedDropdown]);
+        setChartData([
+          {
+            id: 'us',
+            color: 'hsl(145, 70%, 50%)',
+            data: selectedCardData,
+          }
+        ]);
+      } 
+  }, [cardIndex, clickedDropdown]);
+
+  useEffect(() => {
+        setChartData([
+          {
+            id: 'us',
+            color: 'hsl(145, 70%, 50%)',
+            data: selectedCardData,
+          }
+        ]);
+  }, [selectedCardData]);
 
   function handleDropdownClick(event){
 
-    console.log("clicked")
-
     if(event.target.matches('.temperature')){
       setClickedDropdown("hour_temp_f")
-      setSelectedCardData(DataArray[cardIndex][clickedDropdown])
       setAxisLeftLegend("Temperature")
 
     } else if (event.target.matches('.uv-index')) {
         setClickedDropdown("hour_uv")
-        setSelectedCardData(DataArray[cardIndex][clickedDropdown])
         setAxisLeftLegend("UV Index")
 
       } else if (event.target.matches('.wind')) {
         setClickedDropdown("hour_wind_mph")
-        setSelectedCardData(DataArray[cardIndex][clickedDropdown])
         setAxisLeftLegend("Wind (mph)")
 
       } else if (event.target.matches('.precipitation')) {
         setClickedDropdown("hour_precipitation")
-        setSelectedCardData(DataArray[cardIndex][clickedDropdown])
         setAxisLeftLegend("Precipitation (in)")
 
       } else if (event.target.matches('.feels-like')) {
         setClickedDropdown("hour_feelslike_f")
-        setSelectedCardData(DataArray[cardIndex][clickedDropdown])
         setAxisLeftLegend("Temperature")
 
       } else if (event.target.matches('.humidity')) {
         setClickedDropdown("hour_humidity")
-        setSelectedCardData(DataArray[cardIndex][clickedDropdown])
         setAxisLeftLegend("Humidity")
 
       } else if (event.target.matches('.visibility')) {
         setClickedDropdown("hour_visibility_miles")
-        setSelectedCardData(DataArray[cardIndex][clickedDropdown])
         setAxisLeftLegend("Visibility")
 
       } else if (event.target.matches('.pressure')) {
         setClickedDropdown("hour_pressure_in")
-        setSelectedCardData(DataArray[cardIndex][clickedDropdown])
         setAxisLeftLegend("Pressure (in)")
     } 
     
@@ -100,17 +100,6 @@ function App() {
     setForecastDataArray(DataArray)
     setLoading(false);
   }, [locationInput, ForecastDays, DataArray]);
-
-  useEffect(() => {
-    console.log(clickedDropdown)
-    setChartData([
-      {
-        id: 'us',
-        color: 'hsl(145, 70%, 50%)',
-        data: selectedCardData,
-      }
-    ]);
-  }, [selectedCardData, clickedDropdown]);
 
   return (
     <div>
